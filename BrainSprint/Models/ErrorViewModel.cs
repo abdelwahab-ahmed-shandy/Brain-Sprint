77d6e6bdcd51e4ba33b68240c4fa812635ec6e7f
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-04-28 17:44:47 +0300

    New Project
    BrainSprint  => Presentation Layer
    
    DataAccess => Data Layer
    
    Models  =>  Entity/Model Layer
    
    Utility =>  Utilities/Helper Classes

---
+++ BrainSprint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrainSprint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
