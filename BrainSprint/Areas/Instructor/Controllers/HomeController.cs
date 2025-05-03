06d51ff937a822cba764e2eac3eb8292580841d8
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-05-03 06:13:34 +0300

    Add Add Views in Area(Customer)(Instrastractor) => (index ),(index, Create, Courses,analytics) By E/Sama

--- BrainSprint/Areas/Instructor/Controllers/HomeController.cs
+++ BrainSprint/Areas/Instructor/Controllers/HomeController.cs
@@ -4,11 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 namespace BrainSprint.Areas.Instructor.Controllers
 {
     [Area("Instructor")]
-    [Authorize(Roles = "Instructor")]
+    [Authorize(Roles = "Instructor,Admin,SuperAdmin")]
 
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult analytics()
         {
             return View();
         }
--- BrainSprint/obj/Debug/net9.0/BrainSprint.AssemblyInfo.cs
+++ BrainSprint/obj/Debug/net9.0/BrainSprint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainSprint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65929e1a4b7a30a4ce1faaf9e964497917aaeb50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baac890e2ec0bc94ddbd352e5ddb5e1b8ef8d956")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainSprint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainSprint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
